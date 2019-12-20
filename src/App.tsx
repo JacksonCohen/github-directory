a1def00188559e16c37ff4670249e7b84fe49ce9
Jackson <jacksonrcohen@gmail.com>
2019-12-20 00:28:50 -0500

    Implement functionality to authenticate user using oauth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Search from './components/Search';
+import UserProfile from './components/UserProfile';
 
-function App() {
-  return <div className='App'>Test</div>;
+export class App extends Component {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='sign-in'>
+          <a href='https://github.com/login/oauth/authorize?client_id=6f7c14d6f3a82966cbb6'>
+            Sign In Using GitHub
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
