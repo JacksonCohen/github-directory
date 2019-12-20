dd02c255accc75e276d37e263723a70607f472e0
Jackson <jacksonrcohen@gmail.com>
2019-12-19 23:22:56 -0500

    Create skeleton of app

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-
-import './index.css';
-import store from './store';
 
 const render = () => {
   const App = require('./App').default;
 
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root'),
-  );
-}
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
 
 render();
 
